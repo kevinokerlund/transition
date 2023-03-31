92f429c57db9b4f4e793ba2b8de66239b438e7b6
Kevin Okerlund <kevinokerlund@users.noreply.github.com>
2023-03-31 00:15:34 -0600

    storybook (7.0 rc) init

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,22 +4,14 @@ module.exports = {
 		'browser': true,
 		'es2021': true
 	},
-	'extends': [
-		'eslint:recommended',
-		'plugin:react/recommended',
-		'plugin:@typescript-eslint/recommended'
-	],
-	'overrides': [
-	],
+	'extends': ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:storybook/recommended'],
+	'overrides': [],
 	'parser': '@typescript-eslint/parser',
 	'parserOptions': {
 		'ecmaVersion': 'latest',
 		'sourceType': 'module'
 	},
-	'plugins': [
-		'react',
-		'@typescript-eslint'
-	],
+	'plugins': ['react', '@typescript-eslint'],
 	'rules': {
 		'indent': ['error', 'tab'],
 		'linebreak-style': ['error', 'unix'],
