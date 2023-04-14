a83491b011447c92486e6f806f1360ba8dabcfa5
Kevin Okerlund <kevinokerlund@users.noreply.github.com>
2023-04-13 20:19:55 -0600

    configure eslint and jest

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,8 @@
 module.exports = {
 	'env': {
 		'browser': true,
-		'es2021': true
+		'es2021': true,
+		'jest/globals': true
 	},
 	'extends': ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:storybook/recommended'],
 	'overrides': [],
@@ -11,11 +12,16 @@ module.exports = {
 		'ecmaVersion': 'latest',
 		'sourceType': 'module'
 	},
-	'plugins': ['react', '@typescript-eslint'],
+	'plugins': ['react', '@typescript-eslint', 'jest'],
 	'rules': {
 		'indent': ['error', 'tab'],
 		'linebreak-style': ['error', 'unix'],
 		'quotes': ['error', 'single'],
-		'semi': ['error', 'always']
+		'semi': ['error', 'always'],
+		'jest/no-disabled-tests': 'warn',
+		'jest/no-focused-tests': 'error',
+		'jest/no-identical-title': 'error',
+		'jest/prefer-to-have-length': 'warn',
+		'jest/valid-expect': 'error'
 	}
 };
